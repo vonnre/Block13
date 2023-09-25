e072807f0739376d0de4059808852dcacc7a0957
Brittany <111539361+vonnre@users.noreply.github.com>
2023-09-25 02:12:49 -0500

    Add files via upload

---
+++ workshop.js
@@ -0,0 +1,111 @@
+//Is Truthy Tab - use conditional if/else statements to check each input value
+// to get the expected print as the result using console.log
+
+
+// let input = "I am a string";
+
+// if(input){
+//     console.log("true");
+// } else{
+//     console.log("False");
+// }
+
+// let input = false;
+
+// if(input){
+//     console.log("true");
+// } else{
+//     console.log("The boolean value false is falsy");
+// }
+
+// let input = null;
+
+// if(input){
+//     console.log("true");
+// } else{
+//     console.log("The null value is falsy");
+// }
+
+// let input;
+
+// if(input){
+//     console.log("true");
+// } else{
+//     console.log("undefined is falsy");
+// }
+
+// let input = 0;
+
+// if(input){
+//     console.log("true");
+// } else{
+//     console.log("The number 0 is falsy (the only falsy number)");
+// }
+
+// let input = 0;
+
+// if(input){
+//     console.log("true");
+// } else{
+//     console.log("The number 0 is falsy (the only falsy number)");
+// }
+
+// let input = "";
+
+// if(input){
+//     console.log("true");
+// } else{
+//     console.log("The empty string is falsy (the only falsy string)");
+// }
+
+//Number line Tab - use conditional if/else if/else statements to check each input value
+// to get the expected print as the result using console.log. 
+//Assign variables to num1, num2 and result with the let tag
+
+// let num1 = 99;
+// let num2 = -2;
+// let result = num1+num2;
+
+// if(result < -1000) {
+//     console.log(result + " is less than -1000");
+// } else if (result < 0) {
+//     console.log(result + " is a negative number");
+// } else if (result = 0) {
+//     console.log(result + " is equal to 0");
+// } else if (result > 0) {
+//     console.log(result + " is larger than 0");
+// } else if (result > 100) {
+//     console.log(result + " is greater than 100");
+// } else {
+//     console.log(result + " doesn't fit any of the other categories!");
+// };
+
+
+//GREATER THAN OR EQUAL TO 5 tab - use if/else statements with boolean to see if both variables 
+//are greater than or equal to 5. 
+//Assign num1 and num2 variables
+//If both variables are true, print true. Otherwise print false.
+
+// let num1 = 5;
+// let num2 = 5;
+
+// if(num1 >= 5 && num2 >= 5) {
+//     console.log("True");
+// } else {
+//     console.log("false");
+// };
+
+
+//Pair and Compare -  Using if/else statements and strict equality operators, JavaScript code in a script tag that compares 
+// two sets of two values. Print true if at least one of the pairs is truthy.
+
+// const param1A = 11;
+// const param1B = "eleven";
+// const param2A = "four";
+// const param2B = "for";
+
+// if (param1A === param1B ||param2A === param2B) {
+//     console.log ("True");
+// } else {
+//     console.log ("False");
+// }
